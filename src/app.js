5be5ec3c1ef79b7411c1c116cc37b82a5911c744
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2026-03-10 19:11:56 -0600

    Usa express para manejar http

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("Hola serverr");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hola express");
+})
+
+app.get("/alumnos", (req, res) => {
+  res.send("Hola alumnos express");
+})
+
+app.listen(8081);
