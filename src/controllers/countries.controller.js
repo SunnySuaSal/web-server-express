4773c75f9047504b48685cdc1ee8aa058abae9e4
Sunny Suárez Saldaña <yo_soyalegre@hotmail.com>
2026-05-14 02:41:52 -0600

    Mueve lista de paises a servicio

--- src/controllers/countries.controller.js
+++ src/controllers/countries.controller.js
@@ -9,10 +9,262 @@ const getCountries = (req = request, res = response) => { //subruta
 
   const { q } = req.query;
 
-  res.status(200).json({
-    msg: "GET countries",
-    q
-  })
+  const countries = [ //Es convencion que comiencen con guion bajo
+
+    {
+
+    name: 'Albania',
+
+    capital: 'Tirana',
+
+    region: 'Europe',
+
+    population: 2837743,
+
+    flag: 'https://flagcdn.com/al.svg'
+
+  },
+
+  {
+
+    name: 'Argentina',
+
+    capital: 'Buenos Aires',
+
+    region: 'Americas',
+
+    population: 45810000,
+
+    flag: 'https://flagcdn.com/ar.svg'
+
+  },
+
+  {
+
+    name: 'Australia',
+
+    capital: 'Canberra',
+
+    region: 'Oceania',
+
+    population: 26010000,
+
+    flag: 'https://flagcdn.com/au.svg'
+
+  },
+
+  {
+
+    name: 'Brazil',
+
+    capital: 'Brasília',
+
+    region: 'Americas',
+
+    population: 216400000,
+
+    flag: 'https://flagcdn.com/br.svg'
+
+  },
+
+  {
+
+    name: 'Canada',
+
+    capital: 'Ottawa',
+
+    region: 'Americas',
+
+    population: 38900000,
+
+    flag: 'https://flagcdn.com/ca.svg'
+
+  },
+
+  {
+
+    name: 'China',
+
+    capital: 'Beijing',
+
+    region: 'Asia',
+
+    population: 1412000000,
+
+    flag: 'https://flagcdn.com/cn.svg'
+
+  },
+
+  {
+
+    name: 'Egypt',
+
+    capital: 'Cairo',
+
+    region: 'Africa',
+
+    population: 112700000,
+
+    flag: 'https://flagcdn.com/eg.svg'
+
+  },
+
+  {
+
+    name: 'France',
+
+    capital: 'Paris',
+
+    region: 'Europe',
+
+    population: 67000000,
+
+    flag: 'https://flagcdn.com/fr.svg'
+
+  },
+
+  {
+
+    name: 'Germany',
+
+    capital: 'Berlin',
+
+    region: 'Europe',
+
+    population: 83200000,
+
+    flag: 'https://flagcdn.com/de.svg'
+
+  },
+
+  {
+
+    name: 'India',
+
+    capital: 'New Delhi',
+
+    region: 'Asia',
+
+    population: 1429000000,
+
+    flag: 'https://flagcdn.com/in.svg'
+
+  },
+
+  {
+
+    name: 'Japan',
+
+    capital: 'Tokyo',
+
+    region: 'Asia',
+
+    population: 125000000,
+
+    flag: 'https://flagcdn.com/jp.svg'
+
+  },
+
+  {
+
+    name: 'Mexico',
+
+    capital: 'Mexico City',
+
+    region: 'Americas',
+
+    population: 126000000,
+
+    flag: 'https://flagcdn.com/mx.svg'
+
+  },
+
+  {
+
+    name: 'Nigeria',
+
+    capital: 'Abuja',
+
+    region: 'Africa',
+
+    population: 227000000,
+
+    flag: 'https://flagcdn.com/ng.svg'
+
+  },
+
+  {
+
+    name: 'South Africa',
+
+    capital: 'Pretoria',
+
+    region: 'Africa',
+
+    population: 60400000,
+
+    flag: 'https://flagcdn.com/za.svg'
+
+  },
+
+  {
+
+    name: 'Spain',
+
+    capital: 'Madrid',
+
+    region: 'Europe',
+
+    population: 48300000,
+
+    flag: 'https://flagcdn.com/es.svg'
+
+  },
+
+  {
+
+    name: 'United Kingdom',
+
+    capital: 'London',
+
+    region: 'Europe',
+
+    population: 67700000,
+
+    flag: 'https://flagcdn.com/gb.svg'
+
+  },
+
+  {
+
+    name: 'United States',
+
+    capital: 'Washington, D.C.',
+
+    region: 'Americas',
+
+    population: 334900000,
+
+    flag: 'https://flagcdn.com/us.svg'
+
+  },
+
+  {
+
+    name: 'Afghanistan',
+
+    capital: 'Kabul',
+
+    region: 'Asia',
+
+    population: 40218234,
+
+    flag: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg'
+
+  }
+  ]; 
+
+  res.status(200).json(countries)
 }
 
 const createCountry = (req = request, res = response) => { //subruta
